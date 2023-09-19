2a15b0e6cb001630bbe36676aa1fdefe0939d627
Aylin Doğan <101521126+Aylinxd@users.noreply.github.com>
2023-09-19 11:25:57 +0300

    Update pv_add.py
    How to print all monitors with several ways

--- pv_add.py
+++ pv_add.py
@@ -3,41 +3,33 @@ import os
 import pathlib
 
 script_path = os.path.dirname(os.path.abspath(__file__))
-
 dss_file = pathlib.Path(script_path).joinpath("feeders", "13Bus", "IEEE13Nodeckt.dss")
-
 dss = py_dss_interface.DSSDLL()
-
 dss.text(f"compile [{dss_file}]")
+# dss.text("Show voltages")
+# dss.text("Show powers")
 
-#dss.text("Show voltages")
-#dss.text("Show powers")
-
-busses=dss.circuit_all_bus_names()
+busses = dss.circuit_all_bus_names()
 print(busses)
-a=dss.monitors_all_names()
+num = dss.monitors_count()
+
+a = dss.monitors_all_names()
 print(a)
-dss.monitors_write_name(a[5]) #activate the monitor
-mon= dss.monitors_read_name()
-print(mon)
-elements_0f_mon=dss.monitors_read_element()
-print(elements_0f_mon)
+i =0
+while i < num:
+    dss.monitors_write_name(a[i])  # activate the monitor
+    mon = dss.monitors_read_name()
+    print(mon)
+    elements_0f_mon = dss.monitors_read_element()
+    print(elements_0f_mon)
+    print(dss.monitors_num_channels())
+    i += 1
+
+
 dss.monitors_show()
-
-
-dss.circuit_set_active_bus('645')
-pow= dss.cktelement_powers()
-volt=dss.cktelement_voltages()
+#dss.circuit_set_active_element('load.650')
+pow = dss.cktelement_powers()
+volt = dss.cktelement_voltages()
 print(pow)
 print(volt)
-
-
-
-
-
-
-
-
-
-
 
